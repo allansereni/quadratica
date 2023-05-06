94a01425fa35fe1e5dc66c2b00bccc7daa32e6a3
Allan Kanesiro Sereni <112025460+allansereni@users.noreply.github.com>
2023-05-06 13:19:38 -0300

    Atualizando main para receber classes

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-def criar_grupo():
-    print('Criar grupo')
-    global nome_grupo
-    global modalidade
-    global mensalidade_quadra
-    global mensalidade_atleta
-    global avulso_atleta
-    nome_grupo = input("Qual o nome do grupo? \nNome do grupo:")
-    modalidade = input('Modalidade:')
-    mensalidade_quadra = eval(input('Valor da mensalidade da quadra:'))
-    mensalidade_atleta = eval(input(('Valor da mensalidade do atleta')))
-    avulso_atleta = eval(input('Valor avulso do atleta:'))
+from atleta import Atleta
+from grupo import Grupo
 
-def cadastrar_atleta():
-    print("Dados do atleta")
-    global nome_atleta
-    global idade_atleta
-    global posicao_atleta
-    global estrelas_atleta
-    global admin
-    global frequencia_atleta
-    global valor_a_pagar
-    nome_atleta = input('Nome:')
-    idade_atleta = int(input('Idade:'))
-    posicao_atleta = input('Posição:')
-    estrelas_atleta = int(input('Estrelas:'))
-    admin = input('Administrador? (s)sim / (n)não :')
-    frequencia_atleta = input('Mensal ou avulso:')
-    valor_a_pagar = 0
-    if (frequencia_atleta.upper() == 'MENSAL'):
-        valor_a_pagar = mensalidade_atleta
-    else:
-        valor_a_pagar = avulso_atleta
+a = Atleta()
+g = Grupo()
 
 sair = 0
-
 
 while (sair == 0):
     print('************************************************')
@@ -45,8 +16,8 @@ while (sair == 0):
     print('2 - Cadastrar atleta')
     print('3 - Listar grupos')
     print('4 - Listar ateltas')
-    print('5 - excluir atleta')
-    print('6 - Excluir grupo')
+    print('5 - Excluir grupo')
+    print('6 - Excluir atleta')
     print('7 - Sair')
 
     opcao = eval(input('Selecione a opção:'))
@@ -55,24 +26,39 @@ while (sair == 0):
         continue
     elif (opcao == 1):
 
-        criar_grupo()
+        g.cadastra_grupo()
 
     elif (opcao == 2):
 
-        cadastrar_atleta()
+        a.cadastrar_atleta()
 
     elif (opcao == 3):
-        print('Grupo: {}\nModalidade: {}\nMensalidade da quadra: {}\nMensalidade por atleta: {}\n'
-              'Valor avulso por atleta: {}'.format(nome_grupo, modalidade, mensalidade_quadra,
-                                                   mensalidade_atleta, avulso_atleta))
+
+        g.listar_grupo(0)
+
     elif (opcao == 4):
-        print('Grupo: {}\nModalidade: {}\nNome do atleta: {}\nIdade: {}\n''Posição: {}\nEstrelas: {}'
-              '\nFrequência: {}\nValor a pagar: R${}'.format(nome_grupo, modalidade, nome_atleta,
-                                                             idade_atleta, posicao_atleta, estrelas_atleta,
-                                                             frequencia_atleta, valor_a_pagar))
+
+        a.listar_atleta(0)
+
     elif (opcao == 5):
-        print('Atleta excluído!')
+
+        confirma = input("Você tem certeza? (s)sim \ (n)não: ")
+        if (confirma.upper() == "S"):
+            g.excluir_grupo(0)
+        else:
+            continue
+
     elif (opcao == 6):
-        print('Grupo excluído!')
+
+        confirma = input("Você tem certeza? (s)sim \ (n)não: ")
+        if (confirma.upper() == "S"):
+            a.excluir_atleta(0)
+        else:
+            continue
+
     elif (opcao == 7):
-        sair = 1
+        confirma = input("Você tem certeza? (s)sim \ (n)não: ")
+        if (confirma.upper() == "S"):
+            sair = 1
+        else:
+            continue
