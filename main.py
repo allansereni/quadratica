f7593b113959f2bfaf8c050ab8f8577095912e06
Allan Kanesiro Sereni <112025460+allansereni@users.noreply.github.com>
2023-05-08 13:55:04 -0300

    Colocando o main em function e separando menu em function

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 from atleta import Atleta
 from grupo import Grupo
 
-a = Atleta()
-g = Grupo()
+def main():
+    a = Atleta()
+    g = Grupo()
 
-sair = 0
+    sair = 0
 
-while (sair == 0):
+    while (sair == 0):
+        
+        menu()
+        
+        opcao = eval(input('Selecione a opção:'))
+        if (opcao < 1 or opcao > 7):
+            print('Opção inválida! Escolha uma opção válida!')
+            continue
+        elif (opcao == 1):
+
+            g.cadastra_grupo()
+
+        elif (opcao == 2):
+
+            a.cadastrar_atleta()
+
+        elif (opcao == 3):
+
+            g.listar_grupo(0)
+
+        elif (opcao == 4):
+
+            a.listar_atleta(0)
+
+        elif (opcao == 5):
+
+            confirma = input("Você tem certeza? (s)sim \ (n)não: ")
+            if (confirma.upper() == "S"):
+                g.excluir_grupo(0)
+            else:
+                continue
+
+        elif (opcao == 6):
+
+            confirma = input("Você tem certeza? (s)sim \ (n)não: ")
+            if (confirma.upper() == "S"):
+                a.excluir_atleta(0)
+            else:
+                continue
+
+        elif (opcao == 7):
+            confirma = input("Você tem certeza? (s)sim \ (n)não: ")
+            if (confirma.upper() == "S"):
+                sair = 1
+            else:
+                continue
+
+
+def menu():
     print('************************************************')
     print('****   Quadrática - Administre sua quadra   ****')
     print('************************************************')
@@ -19,46 +68,7 @@ while (sair == 0):
     print('5 - Excluir grupo')
     print('6 - Excluir atleta')
     print('7 - Sair')
+    
 
-    opcao = eval(input('Selecione a opção:'))
-    if (opcao < 1 or opcao > 7):
-        print('Opção inválida! Escolha uma opção válida!')
-        continue
-    elif (opcao == 1):
-
-        g.cadastra_grupo()
-
-    elif (opcao == 2):
-
-        a.cadastrar_atleta()
-
-    elif (opcao == 3):
-
-        g.listar_grupo(0)
-
-    elif (opcao == 4):
-
-        a.listar_atleta(0)
-
-    elif (opcao == 5):
-
-        confirma = input("Você tem certeza? (s)sim \ (n)não: ")
-        if (confirma.upper() == "S"):
-            g.excluir_grupo(0)
-        else:
-            continue
-
-    elif (opcao == 6):
-
-        confirma = input("Você tem certeza? (s)sim \ (n)não: ")
-        if (confirma.upper() == "S"):
-            a.excluir_atleta(0)
-        else:
-            continue
-
-    elif (opcao == 7):
-        confirma = input("Você tem certeza? (s)sim \ (n)não: ")
-        if (confirma.upper() == "S"):
-            sair = 1
-        else:
-            continue
+if __name__ == "__main__":
+    main()
